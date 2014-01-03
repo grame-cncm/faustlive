5794998aa9f922a95fb69b45b4216ce5b462ffe6
Sarah Denoux <sdenoux@grame.fr>
2014-01-03 14:26:08 +0100

    remote changes

---
+++ src/FLRemoteManager.cpp
@@ -0,0 +1,15 @@
+//
+//  FLRemoteManager.cpp
+//  
+//
+//  Created by Sarah Denoux on 13/05/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include "FLRemoteManager.h"
+
+
+FLRemoteManager::FLRemoteManager(){}
+FLRemoteManager::~FLRemoteManager(){}
+
+bool init(){}
