58c424edd04e6c869ef0f492d5232819c830aec4
Sarah Denoux <sdenoux@grame.fr>
2014-10-01 15:44:32 +0200

    try to fix preference placement sometimes on left corner

--- src/MainStructure/FLApp.cpp
+++ src/MainStructure/FLApp.cpp
@@ -1499,6 +1499,7 @@ void FLApp::styleClicked(const QString& style){
 //Preference triggered from Menu
 void FLApp::Preferences(){
     
+    centerOnPrimaryScreen(FLPreferenceWindow::_Instance());
     FLPreferenceWindow::_Instance()->exec();
     
     if(fAudioCreator->didSettingChanged()){
