5b7517a5d8ab51fabcca997c7244af61d5f90d85
Sarah Denoux <sdenoux@grame.fr>
2015-05-27 11:55:12 +0200

    add explanations about FLInterfaceManager

--- src/MainStructure/FLInterfaceManager.h
+++ src/MainStructure/FLInterfaceManager.h
@@ -6,6 +6,8 @@
 //
 // This class was added to try questionning the update of the interfaces and their synchronization. 
 // I'm not so sure it has its use right now BUT it could be of some use with all the remote features.
+
+// When there is a QT interface, it is the one that handles the update of all guis but in a case where ther would  be none, it has to be handled differently
 // FLInterfaceManager keeps track of the interfaces registered and can update them, protecting multiple access to the interface list with a Mutex.
 // It is a singleton in order to be easily acccessible from any another class.
 
