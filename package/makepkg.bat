0f4bf48c743d550b4092d3843cbf3fe62ac2f36f
Dominique Fober <fober@grame.fr>
2019-10-08 17:36:47 +0200

    support vs 14 or 15

--- package/makepkg.bat
+++ package/makepkg.bat
@@ -1,7 +1,17 @@
 
+@echo off
 
+set GEN="Visual Studio 14 2015 Win64"
+
+IF [%1]==[]   GOTO PACKAGE
+IF %1==2014   GOTO PACKAGE
+set GEN="Visual Studio 15 2017 Win64"
+
+:PACKAGE
 mkdir build
 cd build
-cmake .. -G "Visual Studio 14 2015 Win64"
+
+@echo on
+cmake .. -G %GEN%
 cpack -g NSIS64
 move FaustLive-*.exe ..
