0c6e6651cb24722c9ffaf1b35ba35c42c3defda9
Dominique Fober <fober@grame.fr>
2019-10-09 14:59:33 +0200

    packaging on windows

---
+++ Build/MakeRelease.bat
@@ -0,0 +1,37 @@
+
+echo off
+
+IF [%2]==[]     GOTO USAGE
+IF %2==2015 (
+    set GEN="Visual Studio 14 2015 Win64"
+    GOTO NEXT
+)
+IF %2==2017 (
+    set GEN="Visual Studio 15 2017 Win64"
+    GOTO NEXT
+)
+GOTO USAGE
+
+:NEXT
+echo "Make sure you're on the correct branch"
+echo "Make sure your repo is uptodate (git pull --rebase)"
+
+set BUILD=%1
+
+echo "###################### Building FaustLive ######################"
+call Make.bat %BUILD%
+
+echo "####################### Building package #######################"
+cd ../package
+call makepkg.bat %2
+cd ../Build
+
+echo DONE
+GOTO END
+
+:USAGE
+echo "Usage: MakeRelease.bat <version> <builddir> <VS version>
+echo "       <builddir>    is the windows build folder"
+echo "       <VS version>  is a visual studio version: 2015 or 2017"
+
+:END
--- package/makepkg.bat
+++ package/makepkg.bat
@@ -1,17 +1,32 @@
 
 @echo off
 
-set GEN="Visual Studio 14 2015 Win64"
+IF [%1]==[]     GOTO USAGE
+IF %1==2015 (
+    set GEN="Visual Studio 14 2015 Win64"
+    GOTO NEXT
+)
+IF %1==2017 (
+    set GEN="Visual Studio 15 2017 Win64"
+    GOTO NEXT
+)
+GOTO USAGE
 
-IF [%1]==[]   GOTO PACKAGE
-IF %1==2014   GOTO PACKAGE
-set GEN="Visual Studio 15 2017 Win64"
+:NEXT
 
-:PACKAGE
-mkdir build
+IF NOT EXIST build mkdir build
 cd build
 
 @echo on
 cmake .. -G %GEN%
 cpack -g NSIS64
+@echo off
 move FaustLive-*.exe ..
+cd ..
+GOTO END
+
+:USAGE
+echo "Usage: makepkg.bat <VS version>
+echo "       <VS version>  is a visual studio version: 2015 or 2017"
+
+:END
